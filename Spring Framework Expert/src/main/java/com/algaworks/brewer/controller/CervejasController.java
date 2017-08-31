644af4b12bd42d42caaa3ecc59be3eaab6e767ee
Rodrigo Solano Marques <rodrigosolanomarques@gmail.com>
2017-08-30 23:06:17 -0300

    9.6 - Usando o th:field

--- Spring Framework Expert/src/main/java/com/algaworks/brewer/controller/CervejasController.java
+++ Spring Framework Expert/src/main/java/com/algaworks/brewer/controller/CervejasController.java
@@ -41,13 +41,14 @@ public class CervejasController {
     @RequestMapping(value = "/cervejas/novo", method = RequestMethod.POST)
     public ModelAndView cadastrar(@Valid Cerveja cerveja, BindingResult result, Model model, RedirectAttributes attributes) {
 
-        if (result.hasErrors()) {
-            return novo(cerveja);
-        }
+//        if (result.hasErrors()) {
+//            return novo(cerveja);
+//        }
 
         attributes.addFlashAttribute("mensagem", "Cerveja salva com sucesso!");
         System.out.println(cerveja.getSku());
-        System.out.println(cerveja.getNome());
+        System.out.println(cerveja.getSabor());
+        System.out.println(cerveja.getOrigem());
         return new ModelAndView("redirect:/cervejas/novo");
     }
 

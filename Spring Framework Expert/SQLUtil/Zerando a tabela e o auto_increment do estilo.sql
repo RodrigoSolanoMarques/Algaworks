2e4294036069edd865ed0629b4f427860e942656
Rodrigo Solano Marques <rodrigosolanomarques@gmail.com>
2017-09-05 23:56:33 -0300

    11.2 - Salvando o estilo com Ajax: diálogo de cadastro rápido

---
+++ Spring Framework Expert/SQLUtil/Zerando a tabela e o auto_increment do estilo.sql
@@ -0,0 +1,4 @@
+SELECT * FROM brewer.estilo;
+delete from brewer.estilo where codigo > 4;
+alter table brewer.estilo auto_increment = 5;
+
--- Spring Framework Expert/src/main/java/com/algaworks/brewer/controller/EstilosController.java
+++ Spring Framework Expert/src/main/java/com/algaworks/brewer/controller/EstilosController.java
@@ -5,11 +5,16 @@ import com.algaworks.brewer.service.CadastroCervejaService;
 import com.algaworks.brewer.service.CadastroEstiloService;
 import com.algaworks.brewer.service.exception.NomeEstiloJaCadastradoException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -42,6 +47,23 @@ public class EstilosController {
         }
         attributes.addFlashAttribute("mensagem", "Estilo salvo com sucesso!");
         return new ModelAndView("redirect:/estilos/novo");
+    }
+
+    @RequestMapping(value = "/estilos", method = RequestMethod.POST, consumes = {MediaType.APPLICATION_JSON_VALUE})
+    public @ResponseBody ResponseEntity<?> salvar(@RequestBody @Valid Estilo estilo, BindingResult result) {
+
+        if (result.hasErrors()){
+            return ResponseEntity.badRequest().body(result.getFieldError("nome").getDefaultMessage());
+        }
+
+        try{
+            estilo = cadastroEstiloService.salvar(estilo);
+        }catch (NomeEstiloJaCadastradoException e){
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+
+        return ResponseEntity.ok(estilo);
+
     }
 
 }

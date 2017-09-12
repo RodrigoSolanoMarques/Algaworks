7da0b6e11dcf9153defa06f014594470679f8760
Rodrigo Solano Marques <rodrigosolanomarques@gmail.com>
2017-09-11 23:03:33 -0300

    14.6 - Criando pastas para salvar fotos

--- Spring Framework Expert/src/main/java/com/algaworks/brewer/controller/FotosController.java
+++ Spring Framework Expert/src/main/java/com/algaworks/brewer/controller/FotosController.java
@@ -1,7 +1,9 @@
 package com.algaworks.brewer.controller;
 
 import com.algaworks.brewer.dto.FotoDTO;
+import com.algaworks.brewer.storage.FotoStorage;
 import com.algaworks.brewer.storage.FotoStorageRunnable;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,13 +12,15 @@ import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 public class FotosController {
+    @Autowired
+    private FotoStorage fotoStorage;
 
     @PostMapping("/fotos")
     public DeferredResult<FotoDTO> upload(@RequestParam("files[]") MultipartFile[] files) {
 
         DeferredResult<FotoDTO> resultado = new DeferredResult<>();
 
-        Thread thread = new Thread(new FotoStorageRunnable(files, resultado));
+        Thread thread = new Thread(new FotoStorageRunnable(files, resultado, fotoStorage));
         thread.start();
 
         return resultado;
--- Spring Framework Expert/src/main/java/com/algaworks/brewer/storage/FotoStorageRunnable.java
+++ Spring Framework Expert/src/main/java/com/algaworks/brewer/storage/FotoStorageRunnable.java
@@ -8,18 +8,17 @@ public class FotoStorageRunnable implements Runnable {
 
     private MultipartFile[] files;
     private DeferredResult<FotoDTO > resultado;
+    private FotoStorage fotoStorage;
 
-    public FotoStorageRunnable(MultipartFile[] files, DeferredResult<FotoDTO> resultado) {
+    public FotoStorageRunnable(MultipartFile[] files, DeferredResult<FotoDTO> resultado, FotoStorage fotoStorage) {
         this.files = files;
         this.resultado = resultado;
+        this.fotoStorage = fotoStorage;
     }
 
     @Override
     public void run() {
-        System.out.println(">>>>>> Files: " + files[0].getSize());
-
-        // TODO: Salvar a foto no sistema de arquivos...
-
+        fotoStorage.salvarTemporariamente(files);
         String nomeFoto = files[0].getOriginalFilename();
         String contentType = files[0].getContentType();
         resultado.setResult(new FotoDTO(nomeFoto, contentType));

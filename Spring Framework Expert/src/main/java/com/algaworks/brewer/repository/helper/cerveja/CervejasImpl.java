25e8822c78a4b248962a2c2ab58a81a185edbfcc
Rodrigo Solano Marques <rodrigosolanomarques@gmail.com>
2017-09-18 22:23:45 -0300

    15.13 - Parâmetros da ordenação

--- Spring Framework Expert/src/main/java/com/algaworks/brewer/repository/helper/cerveja/CervejasImpl.java
+++ Spring Framework Expert/src/main/java/com/algaworks/brewer/repository/helper/cerveja/CervejasImpl.java
@@ -4,13 +4,11 @@ import com.algaworks.brewer.model.Cerveja;
 import com.algaworks.brewer.repository.filter.CervejaFilter;
 import org.hibernate.Criteria;
 import org.hibernate.Session;
-import org.hibernate.criterion.MatchMode;
-import org.hibernate.criterion.Projection;
-import org.hibernate.criterion.Projections;
-import org.hibernate.criterion.Restrictions;
+import org.hibernate.criterion.*;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.StringUtils;
 
@@ -34,9 +32,15 @@ public class CervejasImpl implements CervejasQueries {
         int totalRegistrosPorPagina = pageable.getPageSize();
         int primeiroRegistro = paginaAtual * totalRegistrosPorPagina;
 
-
         criteria.setFirstResult(primeiroRegistro);
         criteria.setMaxResults(totalRegistrosPorPagina);
+
+        Sort sort = pageable.getSort();
+        if (sort != null){
+            Sort.Order order = sort.iterator().next();
+            String property = order.getProperty();
+            criteria.addOrder(order.isAscending() ? Order.asc(property) : Order.desc(property));
+        }
 
         adicionarFiltro(filtro, criteria);
 

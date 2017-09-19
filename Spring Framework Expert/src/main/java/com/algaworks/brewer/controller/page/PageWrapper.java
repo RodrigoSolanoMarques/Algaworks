bd6807126474ae6b06e98762c6d32945d1c0a4b9
Rodrigo Solano Marques <rodrigosolanomarques@gmail.com>
2017-09-18 22:57:49 -0300

    15.14 - Ordenando a pesquisa por SKU ou nome

--- Spring Framework Expert/src/main/java/com/algaworks/brewer/controller/page/PageWrapper.java
+++ Spring Framework Expert/src/main/java/com/algaworks/brewer/controller/page/PageWrapper.java
@@ -1,6 +1,7 @@
 package com.algaworks.brewer.controller.page;
 
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Sort;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import org.springframework.web.util.UriComponentsBuilder;
 
@@ -37,11 +38,44 @@ public class PageWrapper<T> {
         return page.isLast();
     }
 
-    public int getTotal(){
+    public int getTotal() {
         return page.getTotalPages();
     }
 
-    public String urlParaPagina(int pagina){
+    public String urlParaPagina(int pagina) {
         return uriBuilder.replaceQueryParam("page", pagina).build(true).encode().toUriString();
+    }
+
+    public String urlOrdenada(String propriedade) {
+        UriComponentsBuilder uriBuilderOrder = UriComponentsBuilder.fromUriString(uriBuilder.build(true).encode().toUriString());
+
+        String valorSort = String.format("%s,%s", propriedade, inverterDirecao(propriedade));
+
+        return uriBuilderOrder.replaceQueryParam("sort", valorSort).build(true).encode().toUriString();
+    }
+
+    public String inverterDirecao(String propriedade){
+        String direcao = "asc";
+
+        Sort.Order order = page.getSort() != null ? page.getSort().getOrderFor(propriedade) : null;
+        if(order != null){
+            direcao = Sort.Direction.ASC.equals(order.getDirection()) ? "desc" : "asc";
+        }
+
+        return direcao;
+    }
+
+    public boolean descendente(String propriedade){
+        return inverterDirecao(propriedade).equals("asc");
+    }
+
+    public boolean ordenada(String propriedade){
+        Sort.Order order = page.getSort() != null ? page.getSort().getOrderFor(propriedade) : null;
+        if (order == null){
+            return false;
+        }
+
+        return page.getSort().getOrderFor(propriedade) != null ? true : false;
+
     }
 }

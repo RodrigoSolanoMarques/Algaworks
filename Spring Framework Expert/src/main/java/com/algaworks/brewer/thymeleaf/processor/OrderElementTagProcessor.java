107e1759620cac18d8075e3aa0418e59729cb1b8
Rodrigo Solano Marques <rodrigosolanomarques@gmail.com>
2017-09-19 22:13:12 -0300

    15.15 - Componente Thymeleaf para ordenação

--- Spring Framework Expert/src/main/java/com/algaworks/brewer/thymeleaf/processor/OrderElementTagProcessor.java
+++ Spring Framework Expert/src/main/java/com/algaworks/brewer/thymeleaf/processor/OrderElementTagProcessor.java
@@ -1,6 +1,7 @@
 package com.algaworks.brewer.thymeleaf.processor;
 
 import org.thymeleaf.context.ITemplateContext;
+import org.thymeleaf.model.IAttribute;
 import org.thymeleaf.model.IModel;
 import org.thymeleaf.model.IModelFactory;
 import org.thymeleaf.model.IProcessableElementTag;
@@ -8,12 +9,12 @@ import org.thymeleaf.processor.element.AbstractElementTagProcessor;
 import org.thymeleaf.processor.element.IElementTagStructureHandler;
 import org.thymeleaf.templatemode.TemplateMode;
 
-public class MessageElementTagProcessor extends AbstractElementTagProcessor {
+public class OrderElementTagProcessor  extends AbstractElementTagProcessor {
 
-    private static final String NOME_TAG = "message";
+    private static final String NOME_TAG = "order";
     private static final int PRECEDENCIA = 1000;
 
-    public MessageElementTagProcessor(String dialectPrefix) {
+    public OrderElementTagProcessor(String dialectPrefix) {
         super(TemplateMode.HTML, dialectPrefix, NOME_TAG, true, null, false, PRECEDENCIA);
     }
 
@@ -23,9 +24,14 @@ public class MessageElementTagProcessor extends AbstractElementTagProcessor {
 
         IModelFactory modelFactory = iTemplateContext.getModelFactory();
 
+        IAttribute page = iProcessableElementTag.getAttribute("page");
+        IAttribute field = iProcessableElementTag.getAttribute("field");
+        IAttribute text = iProcessableElementTag.getAttribute("text");
+
         IModel model = modelFactory.createModel();
-        model.add(modelFactory.createStandaloneElementTag("th:block", "th:replace", "fragments/MensagemSucesso :: alert"));
-        model.add(modelFactory.createStandaloneElementTag("th:block", "th:replace", "fragments/MensagensErroValidacao :: alert"));
+        model.add(modelFactory.createStandaloneElementTag("th:block",
+                "th:replace",
+                String.format("fragments/Ordenacao :: order (%s, %s, %s)", page.getValue(), field.getValue(), text.getValue())));
 
         iElementTagStructureHandler.replaceWith(model, true);
 

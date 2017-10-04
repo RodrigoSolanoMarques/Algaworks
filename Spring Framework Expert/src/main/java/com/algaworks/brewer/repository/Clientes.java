3264ecf5af7f42f93337845bf3ae4cf157ded2fa
Rodrigo Solano Marques <rodrigosolanomarques@gmail.com>
2017-10-03 23:04:51 -0300

    16.12 - Desafio: Começando a pesquisa de clientes

--- Spring Framework Expert/src/main/java/com/algaworks/brewer/repository/Clientes.java
+++ Spring Framework Expert/src/main/java/com/algaworks/brewer/repository/Clientes.java
@@ -1,12 +1,13 @@
 package com.algaworks.brewer.repository;
 
 import com.algaworks.brewer.model.Cliente;
+import com.algaworks.brewer.repository.helper.cliente.ClientesQueries;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
-public interface Clientes extends JpaRepository<Cliente, Long> {
+public interface Clientes extends JpaRepository<Cliente, Long>, ClientesQueries {
     public Optional<Cliente> findByCpfOuCnpj(String cpfOuCnpj);
 }

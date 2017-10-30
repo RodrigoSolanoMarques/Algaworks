de99b6864c7b6bde6937dd94b0c35a5bbb5ede06
Rodrigo Solano Marques <rodrigosolanomarques@gmail.com>
2017-10-30 21:53:54 -0200

    18.5 - Input e componente para Data

--- Spring Framework Expert/src/main/java/com/algaworks/brewer/config/WebConfig.java
+++ Spring Framework Expert/src/main/java/com/algaworks/brewer/config/WebConfig.java
@@ -15,10 +15,14 @@ import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
 import org.springframework.cache.guava.GuavaCacheManager;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
+import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.MessageSourceResourceBundle;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
+import org.springframework.format.datetime.standard.DateTimeFormatterRegistrar;
 import org.springframework.format.number.NumberStyleFormatter;
 import org.springframework.format.support.DefaultFormattingConversionService;
 import org.springframework.format.support.FormattingConversionService;
@@ -36,6 +40,7 @@ import org.thymeleaf.templatemode.TemplateMode;
 import org.thymeleaf.templateresolver.ITemplateResolver;
 
 import java.math.BigDecimal;
+import java.time.format.DateTimeFormatter;
 import java.util.Locale;
 import java.util.concurrent.TimeUnit;
 
@@ -97,8 +102,16 @@ public class WebConfig extends WebMvcConfigurerAdapter implements ApplicationCon
 
         NumberStyleFormatter bigDecimalFormatter = new NumberStyleFormatter("#,##0.00");
         conversionService.addFormatterForFieldType(BigDecimal.class, bigDecimalFormatter);
+
         NumberStyleFormatter integerFormatter = new NumberStyleFormatter("#,##0");
         conversionService.addFormatterForFieldType(Integer.class, integerFormatter);
+
+        // API de Datas do Java 8
+        DateTimeFormatterRegistrar dateTimeFormatter = new DateTimeFormatterRegistrar();
+        dateTimeFormatter.setDateFormatter(DateTimeFormatter.ofPattern("dd/MM/yyyy"));
+        dateTimeFormatter.registerFormatters(conversionService);
+
+
         return conversionService;
     }
 
@@ -118,5 +131,14 @@ public class WebConfig extends WebMvcConfigurerAdapter implements ApplicationCon
         cacheManager.setCacheBuilder(cacheBuilder);
 
         return cacheManager;
+    }
+
+
+    @Bean
+    public MessageSource messageSource() {
+        ReloadableResourceBundleMessageSource bundle = new ReloadableResourceBundleMessageSource();
+        bundle.setBasename("classpath:/messages");
+        bundle.setDefaultEncoding("UTF-8"); // http://www.utf8-chartable.de/
+        return bundle;
     }
 }

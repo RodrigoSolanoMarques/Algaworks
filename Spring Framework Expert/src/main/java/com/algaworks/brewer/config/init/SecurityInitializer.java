1dc4e866f625a0bc2af45815382265399a0c65f0
Rodrigo Solano Marques <rodrigosolanomarques@gmail.com>
2017-11-02 17:11:47 -0200

    19.12 - Duração da sessão

--- Spring Framework Expert/src/main/java/com/algaworks/brewer/config/init/SecurityInitializer.java
+++ Spring Framework Expert/src/main/java/com/algaworks/brewer/config/init/SecurityInitializer.java
@@ -5,12 +5,18 @@ import org.springframework.web.filter.CharacterEncodingFilter;
 
 import javax.servlet.FilterRegistration;
 import javax.servlet.ServletContext;
+import javax.servlet.SessionTrackingMode;
+import java.util.EnumSet;
 
 public class SecurityInitializer extends AbstractSecurityWebApplicationInitializer {
 
 
     @Override
     protected void beforeSpringSecurityFilterChain(ServletContext servletContext) {
+
+//        servletContext.getSessionCookieConfig().setMaxAge(20); - Tempo máximo da sessão, independente de o usuário está usando ou não.
+        servletContext.setSessionTrackingModes(EnumSet.of(SessionTrackingMode.COOKIE));
+
         FilterRegistration.Dynamic characterEncodingFilter = servletContext.addFilter("encodingFilter",
                 new CharacterEncodingFilter());
 

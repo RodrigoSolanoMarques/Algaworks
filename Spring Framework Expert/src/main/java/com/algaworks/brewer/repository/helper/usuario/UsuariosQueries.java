7d25c6e41d4da0cdd2a37f3a224c709e0912d439
Rodrigo Solano Marques <rodrigosolanomarques@gmail.com>
2017-11-02 15:18:55 -0200

    19.6 - Carregando as permissões do usuário

--- Spring Framework Expert/src/main/java/com/algaworks/brewer/repository/helper/usuario/UsuariosQueries.java
+++ Spring Framework Expert/src/main/java/com/algaworks/brewer/repository/helper/usuario/UsuariosQueries.java
@@ -2,9 +2,12 @@ package com.algaworks.brewer.repository.helper.usuario;
 
 import com.algaworks.brewer.model.Usuario;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UsuariosQueries {
 
     public Optional<Usuario> porEmailEAtivo(String email);
+
+    public List<String> permissoes(Usuario usuario);
 }
--- Spring Framework Expert/src/main/java/com/algaworks/brewer/security/AppUserDetailsService.java
+++ Spring Framework Expert/src/main/java/com/algaworks/brewer/security/AppUserDetailsService.java
@@ -3,17 +3,18 @@ package com.algaworks.brewer.security;
 import com.algaworks.brewer.model.Usuario;
 import com.algaworks.brewer.repository.Usuarios;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
-import java.util.HashSet;
-import java.util.Optional;
+import java.util.*;
 
 @Service
-public class AppUserDetailsService  implements UserDetailsService {
+public class AppUserDetailsService implements UserDetailsService {
 
     @Autowired
     private Usuarios usuarios;
@@ -23,6 +24,17 @@ public class AppUserDetailsService  implements UserDetailsService {
 
         Optional<Usuario> usuarioOptional = usuarios.porEmailEAtivo(email);
         Usuario usuario = usuarioOptional.orElseThrow(() -> new UsernameNotFoundException("Usuário e/ou senha incorretos"));
-        return new User(usuario.getEmail(), usuario.getSenha(), new HashSet<>());
+        return new User(usuario.getEmail(), usuario.getSenha(), getPermissoes(usuario));
+    }
+
+    private Collection<? extends GrantedAuthority> getPermissoes(Usuario usuario) {
+
+        Set<SimpleGrantedAuthority> authorities = new HashSet<>();
+
+        // Lista de permissoes do usuário
+        List<String> permissoes = usuarios.permissoes(usuario);
+        permissoes.forEach(p -> authorities.add(new SimpleGrantedAuthority(p.toUpperCase())));
+
+        return authorities;
     }
 }

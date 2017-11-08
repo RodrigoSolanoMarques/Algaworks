82ce2624f0894e99b6833c36e0c60f53ec62390a
Rodrigo Solano Marques <rodrigosolanomarques@gmail.com>
2017-11-07 22:22:17 -0200

    22.3 - Implementando a pesquisa rápida de clientes

--- Spring Framework Expert/src/main/java/com/algaworks/brewer/repository/Clientes.java
+++ Spring Framework Expert/src/main/java/com/algaworks/brewer/repository/Clientes.java
@@ -5,9 +5,12 @@ import com.algaworks.brewer.repository.helper.cliente.ClientesQueries;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface Clientes extends JpaRepository<Cliente, Long>, ClientesQueries {
     public Optional<Cliente> findByCpfOuCnpj(String cpfOuCnpj);
+
+    List<Cliente> findByNomeStartingWithIgnoreCase(String nome);
 }
